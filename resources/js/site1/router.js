884aa26423601119b26cf946effa241396a5d6e9
刘云 <liuyun@liuyundeMac-mini.local>
2022-01-09 00:02:54 +0800

    fix site1 router bug

--- resources/js/site1/router.js
+++ resources/js/site1/router.js
@@ -1,15 +1,15 @@
 const Home = () => import("../components/layouts/Home");
-const Success = () => import("../components/Success");
-const Checkout = () => import("../components/Checkout");
+const Success = () => import("../components/Success/Index");
+const Checkout = () => import("../components/Checkout/Index");
 const Detail = () => import("../components/Product/Detail");
 const ProductReplyList = () => import("../components/Product/Reply/List");
-const OrderIndex = () => import("../components/Order");
+const OrderIndex = () => import("../components/Order/Index");
 const OrderCreate = () => import("../components/Order/Create");
 const OrderDetail = () => import("../components/Order/Detail");
 const OrderReturnList = () => import("../components/Order/Return/ReturnList");
-const LocationIndex = () => import("../components/Location");
+const LocationIndex = () => import("../components/Location/Index");
 const LocationCreate = () => import("../components/Location/Create");
-const ComplaintIndex = () => import("../components/Complaint");
+const ComplaintIndex = () => import("../components/Complaint/Index");
 
 export default [
     {path: '/', component: Home},
