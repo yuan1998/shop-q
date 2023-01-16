3cb8d825e17fa2df44ac709a3f9740f3e4daa954
刘云 <liuyun@liuyundeMac-mini.local>
2023-01-16 15:03:19 +0800

    添加新的支付通道:百万只回复

--- app/Payable/BaiWanPay.php
+++ app/Payable/BaiWanPay.php
@@ -44,25 +44,34 @@ class BaiWanPay extends FaCaiPay
             'orderId' => static::generateOrderId($order->order_id),
             'orderAmount' => $order->price,
             'channelType' => $payment,
-            'returnUrl' => $returnUrl,
             'notifyUrl' => $notifyUrl,
+            'returnUrl' => $returnUrl,
+            'order_body' => Helper::site_1_config('order_name'),
         ];
 
         $data['sign'] = static::signStr($data, $appsecret, false);
-        $data['order_body'] = Helper::site_1_config('order_name');
+//        $data['order_body'] = Helper::site_1_config('order_name');
 
 //        dd($data);
         try {
-            $client = new Client();
-            $api_url = $payMethod->api_url ?: "http://baiwan.2023fafa.shop/";
-            //http://www.weilaix.cn/optimus/collect/placeOrder
-            if (!$api_url)
-                throw new \Exception('没有配置支付网关!!');
+            $curl = curl_init();
 
-            $response = $client->request('post', "$api_url/api/newOrder", [
-                'form_params' => $data,
-            ]);
-            $body = $response->getBody()->getContents();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'http://baiwan.2023fafa.shop/api/newOrder',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $data,
+            ));
+
+            $body = curl_exec($curl);
+
+            curl_close($curl);
+
             $a = json_decode($body, true);
             $code = data_get($a, 'code');
             if ($code == 200 && $url = data_get($a, 'data.payUrl')) {
